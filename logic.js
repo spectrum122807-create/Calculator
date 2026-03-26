8fcdcf4afe142084c3e661d303529442abe5f99c
spectrum122807-create <spectrum122807@gmail.com>
2026-03-26 19:02:14 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -44,7 +44,7 @@ function handleInput(value) {
     else if (value === "=") {
         try {
             ans = eval(text);
-            ans= Math.round(ans * Math.pow(10,20)) /Math.pow(10,20) ;
+            ans= Math.round(ans * Math.pow(10,2)) /Math.pow(10,2) ;
             text = ans.toString();
             cursorPos = text.length;
         } catch {
